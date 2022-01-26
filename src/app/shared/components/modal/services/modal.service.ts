4f3773605733cb9a6c84ad721b51caa5b5745608
wslmacieira <wslmacieira@outlook.com>
2022-01-26 17:47:41 -0300

    first commit

---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, TemplateRef } from "@angular/core";
+
+@Injectable()
+export class ModalService {
+
+  public open(config: ModalConfig) {
+    console.log('open called');
+    return new ModalRef();
+  }
+
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef {
+
+  public close(): void {
+    console.log('close called');
+  }
+}
