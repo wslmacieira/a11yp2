01e804bbb4574ecc0221f837556f6d6448a4892a
wslmacieira <wslmacieira@outlook.com>
2022-01-26 22:38:13 -0300

    NgTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { TemplateRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, TemplateRef } from '@angular/core';
 import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 import { Component, ViewChild } from '@angular/core';
 
@@ -7,15 +7,27 @@ import { Component, ViewChild } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>
+
+  @ViewChild('template1') public template1: TemplateRef<any>
+  @ViewChild('template2') public template2: TemplateRef<any>
+
+  public selectedTemplate: TemplateRef<any>;
+
   title = 'a11y-p2';
   public firstName = 'Wagner';
   public modalRef: ModalRef;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private cd: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.selectedTemplate = this.template1;
+    this.cd.detectChanges();
+  }
 
   public show(): void {
+    this.selectedTemplate = this.template2;
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
