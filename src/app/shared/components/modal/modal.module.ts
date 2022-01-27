f80e666e15b7c1f4e748178510de65071260a2ef
wslmacieira <wslmacieira@outlook.com>
2022-01-27 15:28:32 -0300

    Suportando Screan Readers

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 import { FocusTrapModule } from './../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from './modal.component';
@@ -7,7 +8,7 @@ import { ModalService } from './services/modal.service';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule, FocusTrapModule],
+  imports: [CommonModule, FocusTrapModule, FocusBackModule],
   exports: [ModalComponent],
   providers: [ModalService]
 })
